0108b9cc5d9592c9b0c95d0c329dbbd6750b3074
RyoBamboo <itakedaka@gmail.com>
2017-11-14 20:54:42 +0900

    test

--- src/api/voice.py
+++ src/api/voice.py
@@ -15,13 +15,14 @@ def upload():
         return jsonify(res='error'), 400
 
     f = request.files['file']
-    f.save(f.filename)
+    data = base64.b64decode(f.read())
 
     # data = request.json
     # print(data)
     # sound = data['sound']['file_data']
     # sound
-    # fp = open('./test.m4a', 'w')
+    fp = open('./test.m4a', 'wb')
+    fp.write(data)
     # fp.write(str(base64.b64decode(sound)))
     # BASE_DIR = os.path.dirname(__file__)
     # myfile = open(os.path.join(BASE_DIR, 'static/sample.m4a'), "w")
