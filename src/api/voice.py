b054b4f77424fbff6f3193eb53f77f3da6cf43a5
RyoBamboo <itakedaka@gmail.com>
2017-11-14 18:15:26 +0900

    test

--- src/api/voice.py
+++ src/api/voice.py
@@ -3,10 +3,8 @@ import base64
 from flask import Blueprint, request, jsonify
 from pprint import pprint
 
-from src import db
 from src.service.response import Response
 
-from src.models.user import User
 
 bp_voice = Blueprint('voice', __name__)
 
@@ -16,11 +14,10 @@ def upload():
     if request.headers['Content-Type'] != 'application/json':
         print("aaa", file=sys.stderr)
         print(request.headers['Content-Type'], file=sys.stderr)
-        return flask.jsonify(res='error'), 400
+        return jsonify(res='error'), 400
 
     data = request.json
     sound = data['sound']['file_data']
     fp = open('./test.m4a', 'w')
     fp.write(str(base64.b64decode(sound)))
-
-	return jsonify(res='ok')
+    return jsonify(res='ok')
