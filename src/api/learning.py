1b76c4be5bb8bd7aff770e339214ee329366e24b
RyoBamboo <itakedaka@gmail.com>
2017-11-16 19:57:58 +0900

    test

--- src/api/learning.py
+++ src/api/learning.py
@@ -21,25 +21,26 @@ bp_learning = Blueprint('learning', __name__)
 def start():
 	# urllib.request.urlretrieve('http://design.prodrb.com/voices/file.wav', 'src/api/bin/tmp/file.wav')
 
-	result = subprocess.Popen(['python', 'src/api/bin/voice_svm_test.py', 'src/api/bin/file.wav'], stdout=subprocess.PIPE)
-	data = result.communicate()[0]
-	decoded_result = data.decode('utf-8').strip()
-	print(decoded_result)
+	# result = subprocess.Popen(['python', 'src/api/bin/voice_svm_test.py', 'src/api/bin/file.wav'], stdout=subprocess.PIPE)
+	# data = result.communicate()[0]
+	# decoded_result = data.decode('utf-8').strip()
+	# print(decoded_result)
+	decoded_result = urllib.request.urlopen('http://design.prodrb.com/result.txt').read()
 
-	# host = 'm14.cloudmqtt.com'
-	# port = 17419
-	# topic = 'test'
-	# username = 'hufvczek'
-	# password = 'Jxv8I_AvjN7S'
+	host = 'm14.cloudmqtt.com'
+	port = 17419
+	topic = 'voice/status'
+	username = 'hufvczek'
+	password = 'Jxv8I_AvjN7S'
 
-	# mqttc = mqtt.Client()
-	# mqttc.on_publish = on_publish
-	# mqttc.username_pw_set(username, password)
-	# mqttc.connect(host, port)
+	mqttc = mqtt.Client()
+	mqttc.on_publish = on_publish
+	mqttc.username_pw_set(username, password)
+	mqttc.connect(host, port)
 
 
-	# mqttc.publish(topic, decoded_result)
-	# mqttc.disconnect()
+	mqttc.publish(topic, decoded_result)
+	mqttc.disconnect()
 
 	
 	return 'ok'
